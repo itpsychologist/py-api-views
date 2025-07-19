0cd1e86372246b7eab266095d0b597a35f39c8ad
VikTOR <dev.psychologist@gmail.com>
2025-07-19 16:24:42 +0300

    Created serializers for all these models

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -17,7 +17,11 @@ router.register("cinema_halls", CinemaHallViewSet, basename="cinema_hall")
 urlpatterns = [
     # Genre routes using APIView functions
     path("genres/", GenreListAPIView.as_view(), name="genre-list"),
-    path("genres/<int:pk>/", GenreDetailAPIView.as_view(), name="genre-detail"),
+    path(
+        "genres/<int:pk>/",
+        GenreDetailAPIView.as_view(),
+        name="genre-detail"
+    ),
 
     # Actor routes using GenericAPIView
     path("actors/", ActorListView.as_view(), name="actor-list"),
